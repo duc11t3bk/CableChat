5fcfa9e45ab66b6ec0532e6c0d58a6123b293ecb
MinhDuc <duc11t3bk@gmail.com>
2018-04-17 15:56:46 +0700

    init

---
+++ app/channels/chat_rooms_channel.rb
@@ -0,0 +1,14 @@
+class ChatRoomsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat_rooms_#{params[:chat_room_id]}_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive data
+    ActionCable.server.broadcast "chat_rooms_#{data["chat_room_id"]}_channel", user: current_user.email,
+      message: data["message"], time_stamp: data["time_stamp"]
+  end
+end
